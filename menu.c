311535553c7a7263acb3d650c9481938ef26ed07
oliviabaudet <olivia.baudet@gmail.com>
2025-05-03 16:59:38 +0200

    mise en place du menu

--- menu.c
+++ menu.c
@@ -4,13 +4,14 @@
 #include "menu.h"
 
 
-
-int menu_filtre() {
-    char chemin[128];
-    t_bmp8 *image = NULL;
-    int quitter = 1;
-    while (quitter) {
-        printf("Veuillez choisir un filtre : \n");
+int menu_filtre(t_bmp8 *image) {
+    int quitter = 0;
+    if (image == NULL) {
+        printf("Aucune image chargée !\n");
+        return -1;
+    }
+    while (!quitter) {
+        printf("\nVeuillez choisir un filtre : \n");
         printf("1. Négatif\n");
         printf("2. Luminosité\n");
         printf("3. Binarisation\n");
@@ -18,48 +19,61 @@ int menu_filtre() {
         printf("5. Flou gaussien\n");
         printf("6. Netteté\n");
         printf("7. Contours\n");
-        printf("8. Relief");
-        printf("9. Retourner au menu précédent\n");
+        printf("8. Retourner au menu précédent\n");
         int selection = -1;
         printf("Votre choix: ");
-        scanf("%d", &selection );
+        scanf("%d", &selection);
+        while ((selection < 1) || (selection > 8)) {
+            printf("Entrez un nombre entre 1 et 8 :\n");
+            scanf("%d", &selection);
+        }
+
         switch (selection) {
             case 1:
-                printf("Chemin du fichier : \n");
-                scanf("%s", chemin);
                 bmp8_negative(image);
-                printf("Filtre appliqué avec succès !");
+                printf("Filtre Négatif appliqué avec succès !\n");
                 break;
-            case 2:
-                printf("Chemin du fichier : \n");
-                scanf("%s", chemin);
-                int value = 50;
+            case 2: {
+                int value;
+                printf("Entrez la valeur de luminosité (-255 à 255) : ");
+                scanf("%d", &value);
+                while ((value < -255) || (value > 255)) {
+                    printf("Veuillez entrer une valeur entre -255 et 255 : ");
+                    scanf("%d", &value);
+                }
                 bmp8_brightness(image, value);
-                printf("Filtre appliqué avec succès !");
+                printf("Filtre Luminosité appliqué avec succès !\n");
                 break;
+            }
             case 3:
-                printf("Chemin du fichier : \n");
-                scanf("%s", chemin);
-                bmp8_threshold(image, 128);
-                printf("Filtre appliqué avec succès !");
+                int value;
+                printf("Entrez la valeur de seuil (0 à 255) : ");
+                scanf("%d", &value);
+                while ((value < 0) || (value > 255)) {
+                    printf("Veuillez entrer une valeur entre 0 et 255 : ");
+                    scanf("%d", &value);
+                }
+                bmp8_threshold(image, value);
+                printf("Filtre Binarisation appliqué avec succès !\n");
                 break;
             case 4:
-                printf("Filtre appliqué avec succès !");
+                // bmp8_applyFilter(image, flouKernel, taille);
+                printf("Filtre Flou (non implémenté ici).\n");
                 break;
             case 5:
-                printf("Filtre appliqué avec succès !");
+                printf("Filtre Flou gaussien (non implémenté ici).\n");
                 break;
             case 6:
-                printf("Filtre appliqué avec succès !");
+                printf("Filtre Netteté (non implémenté ici).\n");
                 break;
             case 7:
-                printf("Filtre appliqué avec succès !");
+                printf("Filtre Contours (non implémenté ici).\n");
                 break;
             case 8:
-                quitter = 0;
+                quitter = 1;
                 break;
             default:
-                printf("Erreur\n");
+                printf("Erreur : choix invalide.\n");
         }
     }
     return 0;
@@ -68,78 +82,66 @@ int menu_filtre() {
 int menu_principal() {
     char chemin[128];
     t_bmp8 *image = NULL;
-    int exit = 1;
-    while (exit) {
-        printf("Veuillez choisir une option :\n");
+    int exit = 0;
+
+    while (!exit) {
+        printf("\nVeuillez choisir une option :\n");
         printf("1. Ouvrir une image\n");
         printf("2. Sauvegarder une image\n");
         printf("3. Appliquer un filtre\n");
         printf("4. Afficher les informations de l'image\n");
         printf("5. Quitter\n");
+
         int choix = -1;
         printf("Votre choix : ");
-        scanf("%d", &choix );
+        scanf("%d", &choix);
+        while (choix < 1 || choix > 5) {
+            printf("Veuillez choisir un nombre entre 1 et 5 : \n");
+            scanf("%d", &choix);
+        }
+
         switch (choix) {
             case 1:
-                printf("Chemin du fichier : \n");
+                printf("Chemin du fichier : ");
                 scanf("%s", chemin);
                 image = bmp8_loadImage(chemin);
                 if (image == NULL) {
-                    printf("Error loading image\n");
-                    return -1;
+                    printf("Erreur de chargement de l'image.\n");
+                } else {
+                    printf("Image chargée avec succès !\n");
                 }
-                printf("Image chargée avec succès !");
                 break;
             case 2:
-                printf("Chemin du fichier : \n");
-                scanf("%s", chemin);
-                bmp8_saveImage(chemin, image);
+                if (image == NULL) {
+                    printf("Aucune image à sauvegarder.\n");
+                } else {
+                    printf("Chemin du fichier de sauvegarde : ");
+                    scanf("%s", chemin);
+                    bmp8_saveImage(chemin, image);
+                    printf("Image sauvegardée avec succès !\n");
+                }
                 break;
             case 3:
-                menu_filtre();
+                if (image == NULL) {
+                    printf("Veuillez d'abord charger une image.\n");
+                } else {
+                    menu_filtre(image);
+                }
                 break;
             case 4:
-                bmp8_printInfo(image);
+                if (image == NULL) {
+                    printf("Aucune image chargée.\n");
+                } else {
+                    bmp8_printInfo(image);
+                }
                 break;
             case 5:
-                exit = 0;
+                printf("Au revoir !\n");
+                exit = 1;
                 break;
             default:
-                printf("Erreur\n");
+                printf("Erreur : choix invalide.\n");
         }
     }
     return 0;
 }
-
-
-// 3. Appliquer un filtre
-// 5. Quitter
-// >>> Votre choix : 3
-// Veuillez choisir un filtre :
-// 1. Négatif
-// 2. Luminosité
-// 3. Binarisation
-// 4. Flou
-// 5. Flou gaussien
-// 5. Netteté
-// 6. Contours
-// 7. Relief
-// 8. Retourner au menu précédent
-// >>> Votre choix : 1
-// Filtre appliqué avec succès !
-// Veuillez choisir une option :
-// 1. Ouvrir une image
-// 2. Sauvegarder une image
-// 3. Appliquer un filtre
-// 4. Afficher les informations de l'image
-// 5. Quitter
-// >>> Votre choix : 2
-// Chemin du fichier : barbara_gray_negatif.bmp
-// Image sauvegardée avec succès !
-// Veuillez choisir une option :
-// 1. Ouvrir une image
-// 2. Sauvegarder une image
-// 3. Appliquer un filtre
-// 4. Afficher les informations de l'image
-// 5. Quitter
-// >>> Votre choix : 5
--- menu.h
+++ menu.h
@@ -1,4 +1,8 @@
 #ifndef MENU_H
 #define MENU_H
+#include "bmp8.h"
+
 int menu_principal(void);
+int menu_filtre(t_bmp8 *image);
+
 #endif //MENU_H
